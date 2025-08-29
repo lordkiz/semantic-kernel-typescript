9b8e38c766fbfaa58fd8da5b740ae7238b606aa2
Kizito Egeonu <kizito.egeonu@gmail.com>
2025-08-29 13:26:21 +0100

    update doc configuration

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   url: "https://lordkiz.github.io/semantic-kernel-typescript",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/semantic-kernel-typescript",
+  baseUrl: process.env.IS_LOCAL ? "/" : "/semantic-kernel-typescript/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
